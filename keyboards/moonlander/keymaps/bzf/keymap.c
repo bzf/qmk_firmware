838ccfc7cd1313f74858e56eb0cbad60b9d880d8
André Ligné <andre@ligne.io>
2022-11-17 13:49:25 +0100

    Add Super and Alt modifiers to home row on hold

--- keyboards/moonlander/keymaps/bzf/keymap.c
+++ keyboards/moonlander/keymaps/bzf/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DVORAK] = LAYOUT_moonlander(
         KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,           KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
         KC_DEL,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    TG(SYMB),       TG(QWERTY),KC_F,    KC_G,    KC_C,    KC_R,    KC_L,     KC_BSLS,
-  CTL_T(KC_ESC), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_HYPR,           KC_MEH,  KC_D,    KC_H,    KC_T,    KC_N,    KC_S,     KC_MINS,
+  CTL_T(KC_ESC), KC_A,    LALT_T(KC_O),    KC_E,    LGUI_T(KC_U),    KC_I,    KC_HYPR,           KC_MEH,  KC_D,    RGUI_T(KC_H),    KC_T,    LALT_T(KC_N),    KC_S,     KC_MINS,
         KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                                KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,     KC_RSFT,
     LT(SYMB,KC_GRV),WEBUSB_PAIR,A(KC_LSFT),KC_LEFT, KC_RGHT,  LALT_T(KC_APP),    RCTL_T(KC_ESC),   KC_UP,   KC_DOWN, KC_SLSH, KC_RBRC,  MO(SYMB),
                                            KC_SPC,  KC_BSPC, KC_LGUI,            KC_LALT,  KC_TAB,  LT(SYMB2, KC_ENT)
